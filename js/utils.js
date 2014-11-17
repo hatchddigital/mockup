45978a73857b55a126edaa29d12072435e224f54
Shaun Norman <shaun@hatchd.com.au>
2014-11-17 11:50:28 +0800

    fixed jshint issue

--- js/utils.js
+++ js/utils.js
@@ -68,11 +68,12 @@ define([
     if (!url) url = window.location.href;
         var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi");
 
+        var hash;
         if (re.test(url)) {
             if (typeof value !== 'undefined' && value !== null)
                 return url.replace(re, '$1' + key + "=" + value + '$2$3');
             else {
-                var hash = url.split('#');
+                hash = url.split('#');
                 url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
                 if (typeof hash[1] !== 'undefined' && hash[1] !== null)
                     url += '#' + hash[1];
@@ -81,8 +82,8 @@ define([
         }
         else {
             if (typeof value !== 'undefined' && value !== null) {
-                var separator = url.indexOf('?') !== -1 ? '&' : '?',
-                    hash = url.split('#');
+                var separator = url.indexOf('?') !== -1 ? '&' : '?';
+                hash = url.split('#');
                 url = hash[0] + separator + key + '=' + value;
                 if (typeof hash[1] !== 'undefined' && hash[1] !== null)
                     url += '#' + hash[1];
